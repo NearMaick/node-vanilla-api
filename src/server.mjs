8c6449812638ab2770e3bc2c8bc88a71c78097bd
Maick Souza <maick_a_s@msn.com>
2022-08-07 11:42:47 -0300

    feat(server): adjusting handling errors

--- src/server.mjs
+++ src/server.mjs
@@ -4,23 +4,31 @@ import { getProductController } from "./controllers/getProductController.mjs";
 import { listProductsController } from "./controllers/listProductsController.mjs";
 
 const server = createServer((request, response) => {
-  switch (request.url) {
-    case "/api/products":
-      if (request.method === "POST") createProductController(request, response);
+  try {
+    switch (request.url) {
+      case "/api/products":
+        if (request.method === "POST")
+          createProductController(request, response);
 
-      if (request.method === "GET") listProductsController(request, response);
-      break;
+        if (request.method === "GET") listProductsController(request, response);
+        break;
 
-    case `${request.url.match(/^\/api\/products\/\d+$/)}`:
-      if (request.method === "GET") {
-        const productId = parseInt(request.url.split("/")[3], 10);
-        getProductController(request, response, productId);
-      }
-      break;
+      case `${request.url.match(/^\/api\/products\/\d+$/)}`:
+        if (request.method === "GET") {
+          const productId = parseInt(request.url.split("/")[3], 10);
+          getProductController(request, response, productId);
+        }
+        break;
 
-    default:
-      response.writeHead(404, { "Content-Type": "text/json" });
-      response.end(JSON.stringify({ message: "Not found" }));
+      default:
+        response.writeHead(404, { "Content-Type": "text/json" });
+        response.end(JSON.stringify({ message: "Not found" }));
+    }
+  } catch (error) {
+    console.log(error);
+
+    response.writeHead(500, { "Content-Type": "text.json" });
+    response.end(JSON.stringify({ message: "Internal server error" }));
   }
 });
 
