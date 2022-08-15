1a87da383e5d3898a9c868d9a33fe20015c4d30c
Maick Souza <maick_a_s@msn.com>
2022-08-15 17:23:25 -0300

    feat(server): header cors

--- src/controllers/listProductsController.js
+++ src/controllers/listProductsController.js
@@ -3,7 +3,10 @@ import { list } from "../repositories/productsRepository.js";
 export async function listProductsController(_, response) {
   try {
     const products = await list();
-    response.writeHead(200, { "Content-Type": "text/json" });
+    response.writeHead(200, {
+      "Content-Type": "text/json",
+      "Access-Control-Allow-Origin": "*",
+    });
     response.end(JSON.stringify(products));
   } catch (error) {
     console.log(error);
